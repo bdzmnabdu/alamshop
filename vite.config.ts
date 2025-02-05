cf62fa1cb17a4fd3a9c5068b042641555dcdf3db
ozamfaqot <ozamlearncode@gmail.com>
2025-02-05 18:30:45 +0700

    Edit vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "node:url";
-// import path from "path";
 import autoprefixer from "autoprefixer";
 import tailwind from "tailwindcss";
 import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   css: {
@@ -22,7 +20,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
-      // "@": path.resolve(__dirname, "src"),
     },
   },
 });
